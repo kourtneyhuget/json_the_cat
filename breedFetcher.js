afb66b70777e062c0085b8033d78e905f7976978
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-09-24 21:45:09 +0000

    created request to catAPI

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,22 @@
 const request = require('request');
+
+const argument = process.argv.slice(2);
+const urlInput = "https://api.thecatapi.com/v1/breeds/search?q=" + argument[0];
+
+request(urlInput, (error, response, body) => {
+  const data = JSON.parse(body);
+  if (error) {
+    console.log("Failed to make request");
+    process.exit;
+  } else if (data.length === 0) {
+    console.log("Requested breed not found");
+  } else {
+    console.log(data[0].description);
+  }
+});
+
+// if (body[0].description === undefined) {
+//   console.log("Requested breed not found");
+// }
+// const data = JSON.parse(body);
+//
