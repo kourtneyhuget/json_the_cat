d99d67bc1436c5d3c168a449cda624b143437681
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-09-25 00:56:37 +0000

    refactored and added index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,13 @@
 const request = require('request');
 
-const argument = process.argv.slice(2);
-const urlInput = "https://api.thecatapi.com/v1/breeds/search?q=" + argument[0];
+function fetchBreedDescription(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    data[0] ? callback(null, data[0].description) : callback("Requested breed not found");
+  });
+};
 
-request(urlInput, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    console.log("Failed to make request");
-    process.exit;
-  } else if (data.length === 0) {
-    console.log("Requested breed not found");
-  } else {
-    console.log(data[0].description);
-  }
-});
+module.exports = { fetchBreedDescription };
